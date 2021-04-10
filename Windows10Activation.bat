0fe9bdba7f64f5f4877b4c3e2a42e907d5fc8474
brettwm204 <44136944+brettwm204@users.noreply.github.com>
2021-04-10 17:50:55 -0400

    Delete Windows10Activation.bat

--- Windows10Activation.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-Echo Enabling PowerShell RemoteSigned
-reg add HKLM\Software\Microsoft\Powershell\1\ShellIds\Microsoft.Powershell /f /v "ExecutionPolicy" /t REG_SZ /d "RemoteSigned"
-mkdir C:\Windows10Activation
-Pause
-Echo Adding Firewall exclusion for "C:\Windows10Activation" dir.
-Powershell.exe Add-MpPreference -ExclusionPath "C:\Windows10Activation"
-Pause
